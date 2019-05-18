60a0282fe7dd79fb519af89516f10eb2b734c579
Vakindu Philliam <Pvakindu@gmail.com>
2019-05-18 16:38:16 +0300

    Add files via upload

---
+++ JavaScript_SetInterval_Anon_Function.js
@@ -0,0 +1,7 @@
+/*JavaScript example to demonstrate the use of SetInterval function. */
+
+setInterval(function(){
+
+document.write("1 Second Interval");
+
+},1000);
---
+++ JavaScript_SetInterval_Arrow_Function.js
@@ -0,0 +1,3 @@
+/*JavaScript example to demonstrate the use of SetInterval function. */
+
+setInterval(()=>document.write('1 Second Interval'),1000);
